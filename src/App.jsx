a3a91dd9d184481db60e55ed9ac7ec9265f6c4f2
SashaGonchar <nekospoon228@gmail.com>
2025-07-25 21:56:01 +0300

    The functionality of adding a candidate to the list has been implemented and a check has been added to avoid existing candidates and empty lines.

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,28 @@ const initialState = {
 
 function reducer(state, action) {
     switch (action.type) {
+        case "reset_votes":
+            return {
+                ...state, candidates: state.candidates.map((candidate) => ({...candidate, votes: 0})),
+            };
+
+        case "update_new_candidate":
+            return {...state, newCandidate: action.payload};
+        case "add_candidate":
+            if(!action.payload.trim() || state.candidates.some((candidate) => candidate.name === action.payload))
+            {
+                return state;
+            }
+            return {...state, candidates: [...state.candidates, {name:action.payload, votes:0}], newCandidate:""};
+
+
         case "dataReceived":
             return {
                 ...state, candidates: action.payload,
                 status: "ready",
             }
         case "dataFailed":
-            return {...state, status:"error"}
+            return {...state, status: "error"}
     }
 }
 
@@ -23,15 +38,14 @@ function VoteTracker() {
 
     useEffect(() => {
         async function fetchData() {
-            try{
+            try {
                 const res = await fetch("http://localhost:9000/candidates");
                 if (!res.ok) {
                     throw new Error("Failed to fetch data")
                 }
                 const data = await res.json();
                 dispatch({type: "dataReceived", payload: data});
-            }
-            catch (error){
+            } catch (error) {
                 dispatch({type: "dataFailed"});
             }
         }
@@ -39,10 +53,10 @@ function VoteTracker() {
         fetchData();
     }, []);
 
-    if(state.status === "loading"){
+    if (state.status === "loading") {
         return <p>Data is still loading, please wait</p>
     }
-    if(state.status === "error"){
+    if (state.status === "error") {
         return <p>Failed to fetch Data</p>
     }
 
@@ -58,16 +72,19 @@ function VoteTracker() {
 
                 ))}
             </ul>
-            <button>Reset Votes</button>
+            <button onClick={() => dispatch({type: "reset_votes"})}>Reset Votes</button>
 
             <div>
                 <h2>Add Candidate</h2>
-                <input type="text" placeholder="Candidate name"/>
-                <button>Add</button>
+                <input type="text" value={state.newCandidate}
+                       placeholder="Candidate name"
+                       onChange={(event) => dispatch({type: "update_new_candidate", payload: event.target.value})}/>
+
+                < button onClick={()=> dispatch({type:"add_candidate", payload: state.newCandidate })}>  Add< /button>
             </div>
         </>
     );
-}
 
+}
 
 export default VoteTracker;
